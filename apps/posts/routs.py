93f02c84e971e2a12567fcd1471818b7f7e24edf
Deepesh27-del <72162939+Deepesh27-del@users.noreply.github.com>
2022-05-02 12:32:12 +0530

    Delete routs.py

--- apps/posts/routs.py
+++
@@ -1,9 +0,0 @@
-
-# imported necessary library
-from flask import Blueprint
-from apps.post.controller import createPost,getPost
-
-
-post_bp = Blueprint('post_bp', __name__)
-post_bp.route('/createPost', methods=['POST'])(createPost)
-post_bp.route('/getPost', methods=['GET'])(getPost)
